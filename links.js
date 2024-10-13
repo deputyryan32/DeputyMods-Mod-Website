0adfc45354b879bed5f80e5008f0d1d96f9db425
DeputyRyan <rybug5210@gmail.com>
2024-10-12 20:57:00 -0400

    V1

---
+++ links.js
@@ -0,0 +1,119 @@
+// links.js
+const modLinks = [
+    {
+      name: "Police AI",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/scripts/48971-policeai/",
+      image: "assets/policeai.png",
+      tags: ["AI", "Plugins"]
+    },
+    {
+      name: "Outdated Deputy Ryan's Pursuit Callouts",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/scripts/48930-outdated-deputyryanspursuitcallouts/",
+      image: "assets/pursuitcallouts.png",
+      tags: ["Callouts", "Outdated"]
+    },
+    {
+      name: "Deputy Ryan's Ultimate Backup",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/datafile/48899-deputyryansultimatebackup/",
+      image: "assets/ultimatebackup.png",
+      tags: ["Ultimate Backup", "XML"]
+    },
+    {
+      name: "Deputy Ryan's FireFX",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/datafile/48513-deputyryansfirefx/",
+      image: "assets/firefx.png",
+      tags: ["Effects"]
+    },
+    {
+      name: "Deputy Ryan's ExplosionFX",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/datafile/48519-deputyryansexplosionfx/",
+      image: "assets/explosionfx.png",
+      tags: ["Effects"]
+    },
+    {
+      name: "Deputy Ryan's Dispatch RDE",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/datafile/48813-deputyryansdispatchrde/",
+      image: "assets/dispatchrde.png",
+      tags: ["Dispatch", "RDE"]
+    },
+    {
+      name: "Deputy Ryan's Enhanced Density",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/datafile/48810-deputyryansenhanceddensity/",
+      image: "assets/enhanceddensity.png",
+      tags: ["Density", "Game Config"]
+    },
+    {
+      name: "Deputy Ryan's STP Questions Recoded",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/datafile/48716-deputyryansstpquestionsrecoded/",
+      image: "assets/stpquestionsrecoded.png",
+      tags: ["Questions", "STP"]
+
+    },
+    {
+      name: "Deputy Ryan's ELS",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/datafile/48715-deputyryansels/",
+      image: "assets/els.png",
+      tags: ["ELS"]
+    },
+    {
+      name: "Deputy Ryan's Pat Down Items",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/datafile/48589-deputyryanspatdownitems/",
+      image: "assets/patdownitems.png",
+      tags: ["STP"]
+    },
+    {
+      name: "Deputy Ryan's Grammar California",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/datafile/48689-deputyryansgrammarcalifornia/",
+      image: "assets/grammarcalifornia.png",
+      tags: ["GrammarPolice", "California"]
+    },
+    {
+      name: "Deputy Ryan's Grammar Generic",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/datafile/48679-deputyryansgrammargeneric/",
+      image: "assets/grammargeneric.png",
+      tags: ["GrammarPolice", "Generic"]
+    },
+    {
+      name: "Retired Deputy Ryan's STP Questioning Reimagined",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/datafile/48684-retired-deputyryansstpquestioningreimagined/",
+      image: "assets/stpquestioningreimagined.png",
+      tags: ["STP"]
+    },
+    {
+      name: "Abandoned Deputy Ryan's Fire Chaos FX",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/datafile/48518-abandoned-deputyryansfirecahosfx/",
+      image: "assets/firechaosfx.png",
+      tags: ["Effects", "Chaos", "Abandoned"]
+    },
+    {
+      name: "Deputy Ryan's CoCharges",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/datafile/48619-deputyryanscocharges/",
+      image: "assets/cocharges.png",
+      tags: ["Charges", "CompuLite"]
+    },
+    {
+      name: "Deputy Ryan's Enhanced Vehicle Items",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/datafile/48546-deputyryansenhancedvehicleitems/",
+      image: "assets/enhancedvehicleitems.png",
+      tags: ["STP"]
+    },
+    {
+      name: "Deputy Ryan's CT Charges/Citations",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/datafile/48614-deputyryansctchargescitations/",
+      image: "assets/ctcharges.png",
+      tags: ["Charges", "CompuLite"]
+    },
+    {
+      name: "Abandoned Deputy Ryan's Liquid FX",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/datafile/48514-abandoned-deputyryansliquidfx/",
+      image: "assets/liquidfx.png",
+      tags: ["Liquid", "Effects", "Abandoned"]
+    },
+    {
+      name: "Deputy Ryan's Visual FX",
+      url: "https://www.lcpdfr.com/downloads/gta5mods/datafile/48528-deputyryansvisualfx/",
+      image: "assets/visualfx.png",
+      tags: ["Visual", "Effects"]
+    }
+  ];
+  
